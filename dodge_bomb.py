a050282c1d1cd3b337a3a2c14414b201b2afa1b9
kobayashi <c0b2306783@edu.teu.ac.jp>
2024-11-26 14:08:20 +0900

    練習1 移動調辞書の導入

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
+DELTA = {
+        pg.K_UP:(0, -5),
+        pg.K_DOWN:(0, 5),
+        pg.K_LEFT:(-5, 0),
+        pg.K_RIGHT:(5, 0),
+        }
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -24,14 +29,20 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        for key,tpl in DELTA.items():  # 辞書に.items()するとキーと値がタプルで取り出せる
+            if key_lst[key]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
+
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
